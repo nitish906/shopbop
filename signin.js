2788c20789506fbb21cd2227df7915d6aad1c8ce
shodevR <royshodev@gmail.com>
2022-05-08 10:31:15 +0530

    pages linked

--- signin.js
+++ signin.js
@@ -14,7 +14,7 @@ if(email=="" || password==""){
 }
   for(var i = 0; i < userdata.length; i++){
       if(email == userdata[i].emai && password == userdata[i].password){
-          window.location.href= "#"
+          window.location.href= "index.html"
           return
       }
   }
